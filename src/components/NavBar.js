f982db3b7add71fd3fa8a69d2f88e92754d8eaee
Dan Jones <108077282+nddpj00@users.noreply.github.com>
2023-08-09 15:50:13 +0100

    initial commit

---
+++ src/components/NavBar.js
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+
+import "./NavBar.css";
+
+function NavBar() {
+  return (
+    <div>
+      <h2>Hello from the navbar</h2>
+    </div>
+  );
+}
+
+export default NavBar;
